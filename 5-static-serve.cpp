63c26e312b5e374b435d09cbaa26845c7693e992
Joseph Haddad <jsh77@zips.uakron.edu>
2015-12-10 15:49:40 -0500

    Clean it up

--- 5-static-serve.cpp
+++ 5-static-serve.cpp
@@ -1,27 +1,23 @@
 #include "lib/network.hpp"
 #include <cstdio>
 
-
-void socket_handler(cs477::net::socket sock)
-{
-	puts("Need to handle a connection!");
+void socket_handler(cs477::net::socket sock) {
+  puts("Need to handle a connection!");
 }
 
 int main(int argc, char **argv) {
-	cs477::net::initialize();
-	auto addr = cs477::net::resolve_address("localhost", 8080);
-	auto host = std::make_shared<cs477::net::acceptor>();
-	host->listen(addr);
+  cs477::net::initialize();
+  auto addr = cs477::net::resolve_address("localhost", 8080);
+  auto host = std::make_shared<cs477::net::acceptor>();
+  host->listen(addr);
 
-	for (int i = 0; i < 32; i++)
-	{
-		host->accept_async(socket_handler);
-	}
+  for (int i = 0; i < 32; i++) {
+    host->accept_async(socket_handler);
+  }
 
-	// Just wait forever.
-	std::promise<void> p;
-	auto f = p.get_future();
-	f.wait();
-	cs477::net::finalize();
-    return 0;
+  std::promise<void> p;
+  auto f = p.get_future();
+  f.wait();
+  cs477::net::finalize();
+  return 0;
 }
--- lib/time.hpp
+++ lib/time.hpp
@@ -4,12 +4,12 @@
 #include <chrono>
 
 inline auto now() {
-	return std::chrono::high_resolution_clock::now();
+  return std::chrono::high_resolution_clock::now();
 }
 
 inline auto to_seconds(std::chrono::time_point<std::chrono::high_resolution_clock> t1, std::chrono::time_point<std::chrono::high_resolution_clock> t2) {
-	std::chrono::duration<double> diff = t2 - t1;
-	return diff.count();
+  std::chrono::duration<double> diff = t2 - t1;
+  return diff.count();
 }
 
 #endif
