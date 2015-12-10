df2ad1fd0d248ecdb5289453adb64357790ca551
Joe Haddad <timer150@gmail.com>
2015-12-10 17:07:26 -0500

    Add basic HTTP server that responds working

--- 5-static-serve.cpp
+++ 5-static-serve.cpp
@@ -1,8 +1,49 @@
 #include "lib/network.hpp"
+#include "lib/http.hpp"
+#include "lib/promise-polyfill.hpp"
 #include <cstdio>
 
+cs477::net::http_response make_response(int status, const std::string &json)
+{
+	cs477::net::http_response rsp;
+	rsp.status = status;
+	switch (status)
+	{
+	case 200:{
+		rsp.message = "Ok";
+		break;
+	}
+	case 404:{
+		rsp.message = "Not Found";
+		break;
+	}
+	case 500:{
+		rsp.message = "Internal Error";
+		break;
+	}
+	}
+
+	if (status == 200)
+	{
+		if (json.length())
+		{
+			rsp.body = json;
+			rsp.headers.emplace_back("Content-Type", "application/json");
+		}
+	}
+
+	return rsp;
+}
+
 void socket_handler(cs477::net::socket sock) {
-  puts("Need to handle a connection!");
+  auto f = cs477::net::read_http_request_async(sock).share();
+  Promise::then(f, [sock](auto s) {
+	  auto rq = s.get();
+	  int status = 200;
+	  std::string result = "Working baby!";
+	  auto rsp = make_response(200, result);
+	  cs477::net::write_http_response_async(sock, rsp);
+  });
 }
 
 int main(int argc, char **argv) {
--- lib/promise-polyfill.hpp
+++ lib/promise-polyfill.hpp
@@ -14,6 +14,31 @@ auto then(std::shared_future<T> fut, F &&f) -> std::future<decltype(f(fut))> {
   std::async(std::launch::async, std::move(l));
   return p->get_future();
 }
+
+template <class Body>
+void task_loop(std::shared_ptr<std::promise<void>> p, Body body)
+{
+	auto s = body().share();
+	then(s, [=](auto f) {
+		try {
+			if (f.get()) task_loop(p, body);
+			else p->set_value();
+		} catch (...) {
+		}
+	});
+}
+
+template <class Body>
+std::future<void> do_while(Body body) {
+	auto p = std::make_shared<std::promise<void>>();
+	std::async(std::launch::async, [=]() {
+		try {
+			task_loop(p, body);
+		} catch (...) {
+		}
+	});
+	return p->get_future();
+}
 };
 
 #endif
