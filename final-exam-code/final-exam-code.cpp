72c11ad3b8cd7934a01d06ad91433eedf88d085b
Joe Haddad <timer150@gmail.com>
2015-12-15 20:55:41 -0500

    Add the actual blurring

--- final-exam-code/final-exam-code.cpp
+++ final-exam-code/final-exam-code.cpp
@@ -83,9 +83,78 @@ int mainExFixed() {
 }
 
 /* 4 completed */
+void conv(matrix &x, const matrix &k) {
+	matrix y;
+	y.create(x.rows + k.rows, x.cols + k.cols);
+
+	const unsigned xR = x.rows, xC = x.cols;
+	parallel_for(0u, xR * xC, [&](auto i) {
+		auto row = i % xR, col = i / xR;
+		auto yrow = row + k.rows / 2;
+		auto ycol = col + k.cols / 2;
+		y(yrow, ycol) = x(row, col);
+	});
+
+	std::atomic<int> weight(0);
+	const unsigned kR = k.rows, kC = k.cols;
+	parallel_for(0u, kR * kC, [&](int i) {
+		auto row = i % kR, col = i / kR;
+		weight += k(row, col);
+	});
+
+	parallel_for(0u, xR * xC, [&](int i) {
+		auto row = i % xR, col = i / xR;
+		int t = 0;
+		auto yrow = row;
+		for (int krow = k.rows - 1; krow >= 0; krow--, yrow++) {
+			auto ycol = col;
+			for (int kcol = k.cols - 1; kcol >= 0; kcol--, ycol++) {
+				t += y(yrow, ycol) * k(krow, kcol);
+			}
+		}
+		if (weight != 0) {
+			t /= weight;
+		}
+		x(row, col) = t;
+	});
+}
+
+int binomial_coefficient(int n, int k) {
+	if (n <= 1 || k == 0) {
+		return 1;
+	}
+	else {
+		return binomial_coefficient(n - 1, k - 1) * n / k;
+	}
+}
+
+matrix binomial(int n) {
+	if ((n & 1) == 0) {
+		throw std::invalid_argument("n must be odd");
+	}
+
+	matrix x, y;
+	x.create(1, n);
+	y.create(n, 1);
+
+	for (int i = 0; i < n / 2; i++) {
+		x(0, i) = x(0, n - i - 1) = binomial_coefficient(n - 1, i);
+		y(i, 0) = y(n - i - 1, 0) = binomial_coefficient(n - 1, i);
+	}
+
+	x(0, n / 2) = binomial_coefficient(n - 1, n / 2);
+	y(n / 2, 0) = binomial_coefficient(n - 1, n / 2);
+
+	return y * x;
+}
+
 concurrent_queue<matrix> pipeline;
 std::future<matrix> blur_image_async(matrix x) {
-  return std::future<matrix>();
+	return std::async(std::launch::async, [](matrix bmp){
+		matrix kernel = binomial(3);
+		conv(bmp, kernel);
+		return std::move(bmp);
+	}, std::move(x));
 }
 
 int main_q4() {
