7ca61526e3ae79cdf3f160e5110c5c91967b0f72
Joseph Haddad <jsh77@zips.uakron.edu>
2015-12-16 13:34:05 -0500

    queue size, not object size

--- homework-6/6-primes-ipc.cpp
+++ homework-6/6-primes-ipc.cpp
@@ -2,15 +2,17 @@
 #include "shared.hpp"
 #include <iostream>
 
-const int PROCESS_COUNT = 3;
+#define PROCESS_COUNT 3
+#define COMPUTE_TO 100000
+#define BLOCK_COUNT (COMPUTE_TO / 1000) + 1
 
-std::shared_ptr<cs477::bounded_queue<primes_request_1000, 4>> rq_queue;
-std::shared_ptr<cs477::bounded_queue<primes_response_1000, 4000>> rp_queue;
+std::shared_ptr<cs477::bounded_queue<primes_request_1000, BLOCK_COUNT>> rq_queue;
+std::shared_ptr<cs477::bounded_queue<primes_response_1000, BLOCK_COUNT>> rp_queue;
 
 int main(int argc, char **argv) {
-	rq_queue = std::make_shared<cs477::bounded_queue<primes_request_1000, 4>>();
+	rq_queue = std::make_shared<cs477::bounded_queue<primes_request_1000, BLOCK_COUNT>>();
 	rq_queue->create("primes-rq-queue");
-	rp_queue = std::make_shared<cs477::bounded_queue<primes_response_1000, 4000>>();
+	rp_queue = std::make_shared<cs477::bounded_queue<primes_response_1000, BLOCK_COUNT>>();
 	rp_queue->create("primes-rp-queue");
 
 	for (auto i = 0; i < PROCESS_COUNT; ++i) {
