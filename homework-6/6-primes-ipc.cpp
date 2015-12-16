926ff92590826ca10570e6571dac20dd228ca2a9
Joe Haddad <timer150@gmail.com>
2015-12-16 02:13:43 -0500

    Add progress for the night

--- homework-6/6-primes-ipc.cpp
+++ homework-6/6-primes-ipc.cpp
@@ -1,6 +1,8 @@
 #include "../lib/shared_mem.hpp"
 #include <iostream>
 
+const int PROCESS_COUNT = 3;
+
 std::shared_ptr<cs477::shm_pool> pool;
 std::shared_ptr<cs477::bounded_queue<cs477::message, 1024>> rq_queue;
 std::shared_ptr<cs477::bounded_queue<cs477::message, 1024>> rp_queue;
@@ -12,6 +14,13 @@ int main(int argc, char **argv) {
 	rq_queue->create("primes-rq-queue");
 	rp_queue = std::make_shared<cs477::bounded_queue<cs477::message, 1024>>();
 	rp_queue->create("primes-rp-queue");
+
+	for (auto i = 0; i < PROCESS_COUNT; ++i) {
+		cs477::buffer prime_block{pool};
+		prime_block.allocate(4);
+		rq_queue->write(prime_block.make_message());
+	}
+
 	std::cout << "test" << std::endl;
 	std::cin.get();
   return 0;
