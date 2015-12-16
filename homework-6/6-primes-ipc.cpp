7dac2754a7d28dba1d29b7d7b44f1f717e201548
Joe Haddad <timer150@gmail.com>
2015-12-16 00:51:32 -0500

    Add message and buffer elements

--- homework-6/6-primes-ipc.cpp
+++ homework-6/6-primes-ipc.cpp
@@ -1,85 +1,18 @@
 #include "../lib/shared_mem.hpp"
-
-struct message {
-  char pool[16];
-  cs477::shm_pool::pointer ptr;
-  uint32_t len;
-};
-
-struct buffer {
-public:
-  buffer()
-      : _pool(nullptr), _size(0) {
-  }
-
-  buffer(std::shared_ptr<cs477::shm_pool> pool)
-      : _pool(pool), _size(0) {
-  }
-
-  buffer(std::shared_ptr<cs477::shm_pool> pool, const message &msg)
-      : _pool(pool) {
-    _ptr = msg.ptr;
-    _size = msg.len;
-  }
-
-public:
-  void allocate(uint32_t len) {
-    auto ptr = _pool->allocate(len);
-
-    if (_ptr != nullptr) {
-      auto src = (*_pool)(_ptr);
-      auto dst = (*_pool)(ptr);
-      memcpy(dst, src, _size);
-
-      _pool->deallocate(_ptr);
-    }
-
-    _ptr = ptr;
-    _size = len;
-  }
-
-  void deallocate() {
-    if (_ptr != nullptr) {
-      _pool->deallocate(_ptr);
-    }
-  }
-
-  message make_message() {
-    message msg;
-    strncpy_s(msg.pool, _pool->name().c_str(), 16);
-    msg.ptr = _ptr;
-    msg.len = _size;
-    return msg;
-  }
-
-public:
-  const char *data() const {
-    return (*_pool)(_ptr);
-  }
-
-  char *data() {
-    return (*_pool)(_ptr);
-  }
-
-  uint32_t size() const {
-    return _size;
-  }
-
-  void shrink(uint32_t size) {
-    assert(size <= _size);
-    _size = size;
-  }
-
-private:
-  std::shared_ptr<cs477::shm_pool> _pool;
-  cs477::shm_pool::pointer _ptr;
-  uint32_t _size;
-};
+#include <iostream>
 
 std::shared_ptr<cs477::shm_pool> pool;
-std::shared_ptr<cs477::bounded_queue<message, 1024>> rq_queue;
-std::shared_ptr<cs477::bounded_queue<message, 1024>> rp_queue;
+std::shared_ptr<cs477::bounded_queue<cs477::message, 1024>> rq_queue;
+std::shared_ptr<cs477::bounded_queue<cs477::message, 1024>> rp_queue;
 
 int main(int argc, char **argv) {
+	pool = std::make_shared<cs477::shm_pool>();
+	pool->create("primes-pool", 4096, 16384);
+	rq_queue = std::make_shared<cs477::bounded_queue<cs477::message, 1024>>();
+	rq_queue->create("primes-rq-queue");
+	rp_queue = std::make_shared<cs477::bounded_queue<cs477::message, 1024>>();
+	rp_queue->create("primes-rp-queue");
+	std::cout << "test" << std::endl;
+	std::cin.get();
   return 0;
 }
